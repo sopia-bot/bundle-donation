5cf2a73696faccf415144d5899125c06c2bde561
raravel <youn@tree-some.dev>
2024-12-10 00:26:56 +0900

    :wrench: log module change to winston

--- apps/background/src/logger.ts
+++ apps/background/src/logger.ts
@@ -1,7 +1,23 @@
-import {pino, Logger} from 'pino';
-import path from 'node:path';
-const logger: Logger = pino(
-    pino.destination(path.join(__pkgdir, 'logs'))
-);
+import path from "path";
+
+const winston = require('winston');
+require('winston-daily-rotate-file');
+
+var transport = new winston.transports.DailyRotateFile({
+  level: 'info',
+  filename: '%DATE%.log',
+  dirname: path.join(__pkgdir, 'logs'),
+  datePattern: 'YYYYMMDD',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '14d'
+});
+
+
+const logger = winston.createLogger({
+  transports: [
+    transport
+  ]
+});
 
 export default logger;
