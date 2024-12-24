3037cc207c5bb6a2a84d0387b352c61bf3356de3
yjyoon <yjyoon@marketlink.co.kr>
2024-12-25 01:13:03 +0900

    :construction: add apply button

--- apps/views/src/components/AppBar.tsx
+++ apps/views/src/components/AppBar.tsx
@@ -4,13 +4,14 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { Tab, Tabs } from '@mui/material';
+import { Button, Divider, IconButton, Snackbar, Tab, Tabs } from '@mui/material';
 import { grey, purple } from '@mui/material/colors';
 import LayersIcon from '@mui/icons-material/Layers';
 import PersonIcon from '@mui/icons-material/Person';
 import AudiotrackIcon from '@mui/icons-material/Audiotrack';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { useNavigate } from 'react-router-dom';
+import { Close } from '@mui/icons-material';
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   alignItems: 'flex-start',
@@ -37,6 +38,26 @@ const TabLabel = styled('div')({
 export default function ProminentAppBar() {
   const [value, setValue] = useState(1);
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const action = (
+    <>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <Close fontSize="small" />
+      </IconButton>
+    </>
+  );
 
   const handleChange = (evt: SyntheticEvent, val: number) => {
     setValue(val);
@@ -65,16 +86,27 @@ export default function ProminentAppBar() {
           >
             🔊 도네이션
           </Typography>
+          <div style={{ flexGrow: 1, alignItems: 'center', textAlign: 'right' }}>
+            <Button variant="contained" color="green" onClick={handleClick}>적용</Button>
+          </div>
         </StyledToolbar>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <StyledTabs value={value} onChange={handleChange}>
-          <Tab label={<TabLabel><LayersIcon/>템플릿</TabLabel>} value={1} />
-          <Tab label={<TabLabel><PersonIcon/>시그니처</TabLabel>} value={2} />
-          <Tab label={<TabLabel><AudiotrackIcon/>효과음</TabLabel>} value={3} />
-          <Tab label={<TabLabel><SettingsIcon/>설정</TabLabel>} value={4} />
-        </StyledTabs>
-      </Box>
+          <StyledTabs value={value} onChange={handleChange}>
+            <Tab label={<TabLabel><LayersIcon/>템플릿</TabLabel>} value={1} />
+            <Tab label={<TabLabel><PersonIcon/>시그니처</TabLabel>} value={2} />
+            <Tab label={<TabLabel><AudiotrackIcon/>효과음</TabLabel>} value={3} />
+            <Tab label={<TabLabel><SettingsIcon/>설정</TabLabel>} value={4} />
+          </StyledTabs>
+        </Box>
       </AppBar>
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        message="적용되었습니다."
+        action={action}
+      />
     </Box>
   );
 }
